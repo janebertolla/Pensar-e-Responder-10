086909e45e6683c34883c82102dc415319015464
jane <jane.bertolla@gmail.com>
2024-06-01 17:51:29 -0300

    alteração na cor: app.js e header.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import Header from "./components/header";
 import ListarTarefa from "./pages/tarefa/ListarTarefa";
+import { unstable_createMuiStrictModeTheme } from "@mui/material";
+import { createTheme } from "@mui/material";
+import React from "react";
+import { ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#1de9b6',
+      },
+      secondary: {
+        main: '#b3e5fc',
+      },
+    },
+  }
+)
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <ListarTarefa />
-    </div>
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <Header />
+          <ListarTarefa />
+        </div>
+      </ThemeProvider>
+    </React.StrictMode>
   );
 }
 
-export default App;
+export default App;
